2e133b306b47181c7c88fb8015dbc96be64b6c4d
Guilherme <guilhermersouza34@gmail.com>
2024-03-04 22:23:00 -0300

    exemplo de profundidade

---
+++ Profundidade.py
@@ -0,0 +1,22 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=" ")
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Exemplo de grafo representado como um dicionário de listas de adjacências
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print("\n\nBusca em Profundidade (DFS):")
+dfs(graph, 'A')
